4f6d836db5c2829da3526368b28627ddb6153110
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2016-07-21 23:35:33 +0100

    rearrange file structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='django-userplus',
+      version='0.1',
+      description='Extended Auth User module for Django',
+      url='https://github.com/ifedapoolarewaju/django-userplus.git',
+      author='Ifedapo Olarewaju',
+      author_email='ifedapoolarewaju@gmail.com',
+      license='MIT',
+      packages=['userplus', 'userplus.lib'],
+      install_requires=['Django>=1.8', 'Django<1.10'],
+      zip_safe=False)
