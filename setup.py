5fa94732b760270c142c94ce7e6d898b00085bc0
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2016-08-29 17:46:10 +0100

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-userplus',
-      version='0.2',
+      version='0.3',
       description='Extended Auth User module for Django',
       url='https://github.com/ifedapoolarewaju/django-userplus.git',
       author='Ifedapo Olarewaju',
@@ -9,4 +9,12 @@ setup(name='django-userplus',
       license='MIT',
       packages=['userplus', 'userplus.lib'],
       install_requires=['Django>=1.9'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Framework :: Django'
+      ],
       zip_safe=False)
